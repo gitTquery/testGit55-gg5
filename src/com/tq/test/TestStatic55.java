d8302adb12d2ebe458d82b51abae8716c2740096
mytquery <mytquery@163.com>
2018-10-25 19:00:53 +0800

    冲突解决

--- src/com/tq/test/TestStatic55.java
+++ src/com/tq/test/TestStatic55.java
@@ -6,11 +6,23 @@ public class TestStatic55 {
 	
 	@Test
 	public  void testtrhdt(){
+		String  strgg="http://testweb.5u51.cn/1030762/gift_change/1102018102414301075200.html?action=2&shareUserId=10400059&sid=4cbe8d4b-bcaf-4953-b6d6-67ac09a68429 ";
+		String  aa="shareUserId=";
+		System.out.println(strgg.substring(strgg.indexOf(aa)+aa.length(),strgg.indexOf("&sid=") ));;
 		
 		String  url="a5u51tzqdz://a5u51/gogiftchangepage?gift_change_url=http://testweb.5u51.cn/1030762/gift_change/1102018102414301075200.html?action=2&shareUserId=10400059 ";
 				String  str="gift_change_url=";
 				String ggg=url.substring(url.indexOf(str)+str.length(), url.length());
 				System.out.println(ggg);
+	}
+	@Test
+	public  void testdsrfhrhdt(){
+		String  aa="client://go_credentialinfo_page?id=1102018102414301075200&share_user_id=";
+		String bb="id=";
+	String  ccc=aa.substring(aa.indexOf(bb)+bb.length(), aa.indexOf("&share_user_id="));
+		System.out.println(ccc);
+		
+		
 	}
 	
 }
